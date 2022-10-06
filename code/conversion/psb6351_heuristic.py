8f0178881e654be4abc0e981a75e4a1b4c264941
Destiny137 <dcarb040@fiu.edu>
2022-10-06 11:28:55 -0400

    homework 1 complete

--- code/conversion/psb6351_heuristic.py
+++ code/conversion/psb6351_heuristic.py
@@ -19,16 +19,17 @@ def infotodict(seqinfo):
     bids_subject_session_prefix: BIDS subject/session prefix
     """
 
-    t1w = create_key('sub-{subject}/anat/sub-{subject}_run-{item}_T1w')
-    dwi = create_key('sub-{subject}/dwi/sub-{subject}_run-{item}_dwi')
-    loc1_task = create_key('')
-    loc2_task = create_key('')
-    study1_task = create_key('sub-{subject}/func/sub-{subject}_task-study_run-1_bold')
-    study2_task = create_key('')
-    study3_task = create_key('')
-    study4_task = create_key('')
-    task_fmap = create_key('')
-    dwi_fmap = create_key('')
+    t1w = create_key('sub-{subject}/ses-1/anat/sub-{subject}_run-{item}_T1w')
+    dwi = create_key('sub-{subject}/ses-1/dwi/sub-{subject}_run-{item}_dwi')
+    loc1_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_loc1_task')
+    loc2_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_loc2_task')
+    study1_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_task-study_run-1_bold')
+    study2_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_task-study_run-2_bold')
+    study3_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_tast-study_run-3_bold')
+    study4_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_test-study_run-4-bold')
+    task_fmap = create_key('sub-{subject}/ses-1/func/sub-{subject}_task_fmap')
+    dwi_fmap = create_key('sub-{subject}/ses-1/func/sub-{subject}_dwi_fmap')
+
 
     info = {t1w : [],
             dwi : [],
@@ -39,23 +40,23 @@ def infotodict(seqinfo):
 
     for s in seqinfo:
         xdim, ydim, slice_num, timepoints = (s[6], s[7], s[8], s[9])
-        if (slice_num == ) and (timepoints == 1) and ("" in s.series_description):
+        if (slice_num == ) and (timepoints == 1) and ("" in protocol_name):
             info[t1w].append(s[2])
-        elif (slice_num > 1) and (timepoints == ) and ("" in s[12]):
+        elif (slice_num > 1) and (timepoints == ) and ("" in protocol_name):
             info[dwi].append(s[2])
-        elif (timepoints == ) and ("ROI_loc_1" in s[12]):
+        elif (timepoints == 304) and ("fMRI_REVL_ROI_loc_1" in protocol_name):
             info[loc1_task].append(s[2])
-        elif (timepoints == ) and ("" in s[12]):
+        elif (timepoints == 303) and ("fMRI_REVL_ROI_loc_2" in protocol_name):
             info[loc2_task].append(s[2])
-        elif (timepoints == 355) and ('' in s[12]):
+        elif (timepoints == 355) and ("fMRI_REVL_Study_1" in protocol_name):
             info[study1_task].append(s[2])
-        elif "dMRI_DistortionMap_AP" in s.series_description:
+        elif "dMRI_DistortionMap_AP" in protocol_name:
             info[dwi_fmap].append({"item": s[2], "dir": "AP"})
-        elif "" in s.series_description:
+        elif "" in protocol_name:
             info[dwi_fmap].append({"item": s[2], "dir": "PA"})
-        elif "" in s.series_description:
+        elif "" in protocol_name:
             info[task_fmap].append({"item": s[2], "dir": "PA"})
-        elif "fMRI_DistortionMap_AP" in s.series_description:
+        elif "fMRI_DistortionMap_AP" in protocol_name:
             info[task_fmap].append({"item": s[2], "dir": "AP"})
         else:
             pass
