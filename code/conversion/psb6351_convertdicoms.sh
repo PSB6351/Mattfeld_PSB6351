4d371ace4183337d2a3c9e39e1b2e3c0c3fabe35
Destiny137 <dcarb040@fiu.edu>
2022-12-01 12:43:42 -0500

    hw3

--- code/conversion/psb6351_convertdicoms.sh
+++ code/conversion/psb6351_convertdicoms.sh
@@ -7,9 +7,5 @@
 #SBATCH --account acc_psb6351
 #SBATCH --partition classroom
 
-#<<<<<<< HEAD
-heudiconv -d '/home/dcarb040/Mattfeld_PSB6351/sourcedata/Mattfeld_REVL-000-vCAT-{subject}-S1/*/*/*/*/*/*' -s 021 -c dcm2niix  -f psb6351_heuristic.py -o /home/dcarb040/Mattfeld_PSB6351/dset -b --minmeta
-#=======
-#heudiconv -d '/home/amattfel/Mattfeld_PSB6351/sourcedata/Mattfeld_REVL-000-vCAT-{subject}-S1/*/*/*/*/*/*' -b --minmeta -s 021 -c dcm2niix -f /home/amattfel/Mattfeld_PSB6351/code/conversion/psb6351_heuristic.py -o /home/amattfel/Mattfeld_PSB6351/dset
-#>>>>>>> 47ac7b23076b2ab2edf118a353c13b2e239ef5c4
+heudiconv -d '/home/dcarb040/Mattfeld_PSB6351/sourcedata/Mattfeld_REVL-000-vCAT-{subject}-S1/*/*/*/*/*/*' -b --minmeta -s 021 -c dcm2niix  -f /home/dcarb040/Mattfeld_PSB6351/code/conversion/psb6351_heuristic.py -o /home/dcarb040/Mattfeld_PSB6351/dset 
 
